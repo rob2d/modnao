902e8c35ab74ef657c35ab752d820c74212976ab
Robert Concepcion III <robert.concepcion.iii@gmail.com>
2023-04-26 17:00:56 -0400

    import mui, theme setup & hello world
    Theme is not customized yet; just needed to get ThemeProvider in to set up Roboto with nextjs/google interop

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,24 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import '@/styles/globals.css';
+import type { AppProps } from 'next/app';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import { Roboto } from 'next/font/google';
+
+const roboto = Roboto({ 
+  subsets: ['latin'],
+  weight: ['300', '400', '500'] 
+});
+
+
+const theme = createTheme({
+  typography: {
+      fontFamily: roboto.style.fontFamily,
+  }
+});
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ThemeProvider theme={theme}>
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
 }
