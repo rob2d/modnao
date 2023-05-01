515a8478f5df5be54645a29dc5caadc8bce5adf1
Robert Concepcion III <robert.concepcion.iii@gmail.com>
2023-05-01 17:12:10 -0400

    extract stage file model pointers in cient

---
+++ src/store/stage-data/extract-stage-data/scanModelsTable.ts
@@ -0,0 +1,24 @@
+import ramToRawAddress from './ramToRawAddress';
+
+/**
+ * @returns an array of pointers for each available
+ * model discovered
+ */
+export default function scanModelsTable(buffer: Buffer) {
+  const modelTablePointer = ramToRawAddress(buffer.readUInt32LE(0x0000));
+  const modelCount = buffer.readUInt32LE(0x0004);
+  const modelPointers: number[] = [];
+
+  for (let i = 0; i < modelCount; i++) {
+    const pointer = modelTablePointer + 4 * i;
+
+    try {
+      modelPointers.push(ramToRawAddress(buffer.readUInt32LE(pointer)));
+    } catch (error) {
+      console.error(`Error: error occurred scanning model at index ${i}`);
+      console.error(error);
+    }
+  }
+
+  return modelPointers;
+}
--- src/store/stage-data/extractStageFile.ts
+++ src/store/stage-data/extractStageFile.ts
@@ -1,4 +1,5 @@
 import { NLStageModel } from '../stageDataSlice';
+import scanModelsTable from './extract-stage-data/scanModelsTable';
 
 export default async function processStageFile(stageFile: File): Promise<{
   models: NLStageModel[];
@@ -7,7 +8,9 @@ export default async function processStageFile(stageFile: File): Promise<{
 
   // @TODO: process buffer into models using logic from node util
   // wait just to simulate something happening for now
-  return new Promise((resolve, reject) => {
+  return new Promise((resolve) => {
+    const modelPointers = scanModelsTable(buffer);
+    console.log('modelPointers discovered ->', modelPointers);
     setTimeout(() => resolve({ models: [] }), 5000);
   });
 }
