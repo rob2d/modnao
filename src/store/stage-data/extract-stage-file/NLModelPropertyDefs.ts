5cf4da0db495ed17d2e2256dc6b70415a1e9a276
Robert Concepcion III <robert.concepcion.iii@gmail.com>
2023-05-04 23:15:39 -0400

    populate property definitions for NLMesh

--- src/store/stage-data/extract-stage-file/NLModelPropertyDefs.ts
+++ src/store/stage-data/extract-stage-file/NLModelPropertyDefs.ts
@@ -39,36 +39,78 @@ export const NLMeshPropertyDefs: NLModelPropertyDef<NLMesh>[] = [
   {
     targetOffset: 0x08,
     readOp: [readFloatLE, readFloatLE, readFloatLE],
-    update: (mesh: NLMesh, values: number[]) => {
+    update(mesh: NLMesh, values: number[]) {
       // determine what to update in NLMesh
     }
   },
   {
     targetOffset: 0x0a,
     readOp: readUInt8,
-    update: (model: NLMesh, value: number) => {
+    update(model: NLMesh, value: number) {
       model.textureWrappingValue = value;
     }
   },
   {
     targetOffset: 0x0c,
     readOp: readUInt32LE,
-    update: (model: NLMesh, value: number) => {
+    update(model: NLMesh, value: number) {
       model.textureControlValue = value;
     }
   },
   {
     targetOffset: 0x0f,
     readOp: readUInt8,
-    update: (model: NLMesh, value: number) => {
+    update(model: NLMesh, value: number) {
       model.textureColorFormat = value;
     }
   },
   {
     targetOffset: 0x0f,
     readOp: readUInt8,
-    update: (model: NLMesh, value: number) => {
+    update(model: NLMesh, value: number) {
       model.textureColorFormat = value;
+    }
+  },
+  {
+    targetOffset: 0x10,
+    readOp: [readFloatLE, readFloatLE, readFloatLE],
+    update(mesh: NLMesh, values: number[]) {
+      mesh.position = values as NLPoint3D;
+    }
+  },
+  {
+    targetOffset: 0x20,
+    readOp: readUInt8,
+    update(mesh: NLMesh, value: number) {
+      mesh.textureNumber = value;
+    }
+  },
+  {
+    targetOffset: 0x24,
+    readOp: readUInt32LE,
+    update(mesh: NLMesh, value: number) {
+      mesh.specularLightValue = value;
+    }
+  },
+  {
+    targetOffset: 0x2c,
+    readOp: readFloatLE,
+    update(mesh: NLMesh, value: number) {
+      mesh.alpha = value;
+    }
+  },
+  {
+    targetOffset: 0x30,
+    readOp: [readFloatLE, readFloatLE, readFloatLE],
+    update(model: NLMesh, values: number[]) {
+      model.color = values as [number, number, number];
+    }
+  },
+  {
+    targetOffset: 0x4c,
+    readOp: readUInt32LE,
+    update(mesh: NLMesh, value: number) {
+      mesh.polygonDataLength = value;
     }
   }
 ];
