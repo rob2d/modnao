54c06db152196a42408dc6ccc8c22e8281be6e12
Robert Concepcion III <robert.concepcion.iii@gmail.com>
2023-05-07 13:40:37 -0400

    red polygons containing ref mode addressing
    a bit easier for debugging

--- src/components/scene/RenderedPolygon.tsx
+++ src/components/scene/RenderedPolygon.tsx
@@ -15,39 +15,48 @@ export default function RenderedPolygon({
   isSelected: boolean;
   index: number;
 }) {
-  console.log('RE RENDERING POLYGON');
   // @TODO: use preact/signal and computed here
   // for same effect and potential DOM optimization
-  const [vertices, indices, displayPosition] = useMemo(() => {
-    const vArray: number[] = [];
-    const iArray: number[] = [];
+  const [vertices, indices, displayPosition, usesReferenceMode] =
+    useMemo(() => {
+      let discoveredRefMode = false;
+      const vArray: number[] = [];
+      const iArray: number[] = [];
 
-    // display position is an aggregated weight
-    // @TODO: precalculate
-    let dArray: Point3D = [0, 0, 0];
+      // display position is an aggregated weight
+      // @TODO: precalculate
+      let dArray: Point3D = [0, 0, 0];
 
-    vertexes.forEach((v, i) => {
-      v.position.forEach((v, i) => {
-        vArray.push(v);
-        dArray[i] += v;
+      vertexes.forEach((v, i) => {
+        if (v.addressingMode === 'reference') {
+          discoveredRefMode = true;
+        }
+        v.position.forEach((v, i) => {
+          vArray.push(v);
+          dArray[i] += v;
+        });
+
+        iArray.push(i);
+
+        if (vertexGroupMode === 'regular') {
+          if (i + 1 < vertexes.length) {
+            iArray.push(i + 1);
+          }
+          if (i + 2 < vertexes.length) {
+            iArray.push(i + 2);
+          }
+        }
       });
 
-      iArray.push(i);
+      dArray = dArray.map((c) => Math.round(c / vertexes.length)) as Point3D;
 
-      if (vertexGroupMode === 'regular') {
-        if (i + 1 < vertexes.length) {
-          iArray.push(i + 1);
-        }
-        if (i + 2 < vertexes.length) {
-          iArray.push(i + 2);
-        }
-      }
-    });
-
-    dArray = dArray.map((c) => Math.round(c / vertexes.length)) as Point3D;
-
-    return [new Float32Array(vArray), new Uint16Array(iArray), dArray];
-  }, [vertexes, vertexGroupMode]);
+      return [
+        new Float32Array(vArray),
+        new Uint16Array(iArray),
+        dArray,
+        discoveredRefMode
+      ];
+    }, [vertexes, vertexGroupMode]);
 
   const lineWidth = isSelected ? 3 : 2;
 
@@ -66,7 +75,13 @@ export default function RenderedPolygon({
       <meshBasicMaterial
         wireframe
         wireframeLinewidth={lineWidth}
-        color={color}
+        /** printing any meshes using Reference mode in red
+         * for tracking down unparseable vertexes temporarily
+         *
+         * @TODO remove this when reference addressing mode
+         * instances discovered
+         */
+        color={!usesReferenceMode ? color : '#F00'}
       />
       <bufferGeometry attach={'geometry'}>
         <bufferAttribute
