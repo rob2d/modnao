9dd773edb5d4df9360291f25d4e7398f4848a049
Robert Concepcion III <robert.concepcion.iii@gmail.com>
2023-05-12 19:40:55 -0400

    clean up debug statements

--- src/store/stage-data/processStageTextureFile.ts
+++ src/store/stage-data/processStageTextureFile.ts
@@ -4,17 +4,7 @@ export default async function processStageTextureFile(
 ): Promise<{ models: NLModel[] }> {
   const buffer = Buffer.from(await textureFile.arrayBuffer());
 
-  models.forEach((model) => {
-    model.meshes.forEach((m) => {
-      console.log('textureNumber ->', m.textureNumber);
-      console.log('textureColorFormat ->', m.textureColorFormat);
-      console.log('textureWrappingFlags ->', m.textureWrappingFlags);
-      console.log('textureSizeValue ->', m.textureSizeValue);
-      console.log('textureSize ->', m.textureSize);
-    });
-  });
-
-  // @TODO cross reference teture file buffer stream containing
+  // @TODO cross reference texture file buffer stream containing
   // pixel data with pre-loaded models. Model contains texture data
 
   return Promise.resolve({ models });
