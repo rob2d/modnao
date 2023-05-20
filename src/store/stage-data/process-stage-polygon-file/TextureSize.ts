0cde70b3a062e2a182031aaf4126d274b212e2ca
Robert Concepcion III <robert.concepcion.iii@gmail.com>
2023-05-20 00:10:15 -0400

    TextureDimension -> TextureDimensionSize

--- src/store/stage-data/process-stage-polygon-file/TextureSize.ts
+++ src/store/stage-data/process-stage-polygon-file/TextureSize.ts
@@ -1,3 +1,3 @@
-export type TextureDimension = 8 | 16 | 32 | 64 | 128 | 256 | 512 | 1024;
+export type TextureDimensionSize = 8 | 16 | 32 | 64 | 128 | 256 | 512 | 1024;
 
-export type TextureSize = [TextureDimension, TextureDimension];
+export type TextureSize = [TextureDimensionSize, TextureDimensionSize];
