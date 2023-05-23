baee72fa1f831d42b35f56d2ad9e1e9899bf1115
Robert Concepcion III <robert.concepcion.iii@gmail.com>
2023-05-22 21:04:41 -0400

    add vercel analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,6 +5,7 @@ import { Provider } from 'react-redux';
 import { wrapper } from '../store/store';
 import useModedTheme from '@/theming/useModedTheme';
 import { ViewOptionsContextProvider } from '@/contexts/ViewOptionsContext';
+import { Analytics } from '@vercel/analytics/react';
 
 export default function App({ Component, ...rest }: AppProps) {
   const theme = useModedTheme();
@@ -17,6 +18,12 @@ export default function App({ Component, ...rest }: AppProps) {
           <Component {...props} />
         </ThemeProvider>
       </Provider>
+      <Analytics
+        mode={
+          process.env.NODE_ENV === 'production' ? 'production' : 'development'
+        }
+      />
+      ;
     </ViewOptionsContextProvider>
   );
 }
