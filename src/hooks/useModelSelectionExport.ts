2bcb044e9585d9525fd60adc4664d90a578300f3
Robert Concepcion III <robert.concepcion.iii@gmail.com>
2023-05-29 18:52:25 -0400

    fixes for export w object key/polygon selection

--- src/hooks/useModelSelectionExport.ts
+++ src/hooks/useModelSelectionExport.ts
@@ -1,29 +1,44 @@
 import { useCallback, useMemo } from 'react';
 import exportFromJSON from 'export-from-json';
-import { selectModel, selectModelIndex, selectObjectKey } from '@/store';
-import { useSelector } from 'react-redux';
+import {
+  selectModel,
+  selectModelIndex,
+  selectObjectKey,
+  selectObjectSelectionType,
+  useAppSelector
+} from '@/store';
 
 export default function useModelSelectionExport() {
-  const model = useSelector(selectModel);
-  const modelIndex = useSelector(selectModelIndex);
-  const objectKey = useSelector(selectObjectKey);
+  const model = useAppSelector(selectModel);
+  const modelIndex = useAppSelector(selectModelIndex);
+  const objectKey = useAppSelector(selectObjectKey);
+  const objectType = useAppSelector(selectObjectSelectionType);
 
   const data = useMemo(() => {
     if (!model) {
       return {};
     }
-    if (objectKey === -1) {
+    if (objectKey === undefined) {
       return model;
     }
 
-    return model.meshes[objectKey];
-  }, [model, objectKey]);
+    if (objectType === 'mesh') {
+      return model.meshes[Number(objectKey)];
+    }
+
+    // type === 'polygon'
+    const [meshKey, polygonKey] = objectKey.split('-').map(Number) as [
+      number,
+      number
+    ];
+    return model.meshes[Number(meshKey)].polygons[Number(polygonKey)];
+  }, [model, objectKey, objectType]);
 
   const onDownloadModelSelection = useCallback(() => {
     if (modelIndex === -1) {
       return;
     }
-    const hasSelection = objectKey !== -1;
+    const hasSelection = objectKey !== undefined;
     exportFromJSON({
       data,
       fileName: `model-${modelIndex}${!hasSelection ? '' : `-${objectKey}`}`,
