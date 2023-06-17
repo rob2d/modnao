b80908f10c18225ddd40ef5cacffe6efe4ed6e61
Robert Concepcion III <robert.concepcion.iii@gmail.com>
2023-06-17 10:59:18 -0400

    "Apply to All" texture functionality

--- src/components/panel/textures/GuiPanelTextureColorOptions.tsx
+++ src/components/panel/textures/GuiPanelTextureColorOptions.tsx
@@ -1,16 +1,25 @@
 import { useCallback, useEffect, useLayoutEffect, useState } from 'react';
-import { Button, List, ListItem, ListSubheader, styled } from '@mui/material';
+import {
+  Button,
+  List,
+  ListItem,
+  ListSubheader,
+  Tooltip,
+  styled
+} from '@mui/material';
 import { useDebounce } from 'use-debounce';
 import HslValues from '@/utils/textures/HslValues';
 import {
   adjustTextureHsl,
   selectEditedTextures,
+  selectTextureDefs,
   useAppDispatch,
   useAppSelector
 } from '@/store';
 import GuiPanelMenuSlider from '../GuiPanelMenuSlider';
 import Icon from '@mdi/react';
 import { mdiRefresh } from '@mdi/js';
+import { batch } from 'react-redux';
 
 const StyledButton = styled(Button)(
   ({ theme }) =>
@@ -31,6 +40,7 @@ export default function GuiPanelTextureColorOptions({
   textureIndex: number;
 }) {
   const dispatch = useAppDispatch();
+  const textureDefs = useAppSelector(selectTextureDefs);
   const editedTextures = useAppSelector(selectEditedTextures);
   const [hsl, setHsl] = useState<HslValues>(() => {
     if (!editedTextures[textureIndex]?.hsl) {
@@ -61,6 +71,18 @@ export default function GuiPanelTextureColorOptions({
   }, [debouncedHsl]);
 
   const onResetValues = useCallback(() => setHsl(DEFAULT_HSL), [setHsl]);
+
+  const onApplyToAll = useCallback(() => {
+    batch(() => {
+      for (
+        let textureIndex = 0;
+        textureIndex < textureDefs.length;
+        textureIndex++
+      ) {
+        dispatch(adjustTextureHsl({ hsl, textureIndex }));
+      }
+    });
+  }, [hsl, textureIndex]);
 
   const hasChanges =
     DEFAULT_HSL.h !== hsl.h ||
@@ -100,16 +122,39 @@ export default function GuiPanelTextureColorOptions({
       />
       {!hasChanges ? undefined : (
         <ListItem>
-          <StyledButton
-            onClick={onResetValues}
-            color='secondary'
-            size='small'
-            variant='outlined'
-            fullWidth
+          <Tooltip
+            title='Reset color changes to this texture'
+            placement='left-start'
           >
-            <Icon path={mdiRefresh} size={1} />
-            RESET
-          </StyledButton>
+            <StyledButton
+              onClick={onResetValues}
+              color='secondary'
+              size='small'
+              variant='outlined'
+              fullWidth
+            >
+              <Icon path={mdiRefresh} size={1} />
+              RESET
+            </StyledButton>
+          </Tooltip>
+        </ListItem>
+      )}
+      {!hasChanges ? undefined : (
+        <ListItem>
+          <Tooltip
+            title='Apply color changes to all loaded textures'
+            placement='left-start'
+          >
+            <StyledButton
+              onClick={onApplyToAll}
+              color='secondary'
+              size='small'
+              variant='outlined'
+              fullWidth
+            >
+              APPLY TO ALL
+            </StyledButton>
+          </Tooltip>
         </ListItem>
       )}
     </List>
