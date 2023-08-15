4bbc1ac6f2d25ef940810d0956fac811edea3af4
Robert Concepcion III <robert.concepcion.iii@gmail.com>
2023-08-15 16:29:45 -0400

    add react-crop library, start setting up dialog

--- src/components/dialogs/fit-image/FitImage.tsx
+++ src/components/dialogs/fit-image/FitImage.tsx
@@ -1,3 +1,33 @@
+import { useCallback, useState } from 'react';
+import Cropper, { Area } from 'react-easy-crop';
+
 export default function FitImageDialog() {
-  return <></>;
+  const [crop, setCrop] = useState({ x: 0, y: 0 });
+  const [rotation, setRotation] = useState(0);
+  const [zoom, setZoom] = useState(1);
+  const [croppedAreaPixels, setCroppedAreaPixels] = useState<Area>();
+  const [croppedImage, setCroppedImage] = useState<string>();
+
+  const onCropComplete = useCallback(
+    (croppedArea: Area, croppedAreaPixels: Area) => {
+      setCroppedAreaPixels(croppedAreaPixels);
+    },
+    []
+  );
+
+  return (
+    <>
+      <div className='crop-container'>
+        <Cropper
+          image='https://img.huffingtonpost.com/asset/5ab4d4ac2000007d06eb2c56.jpeg?cache=sih0jwle4e&ops=1910_1000'
+          crop={crop}
+          zoom={zoom}
+          aspect={4 / 3}
+          onCropChange={setCrop}
+          onCropComplete={onCropComplete}
+          onZoomChange={setZoom}
+        />
+      </div>
+    </>
+  );
 }
