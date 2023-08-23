7c18fd09389dd4bc15091cc62a724996eb14b2b6
Robert Concepcion III <robert.concepcion.iii@gmail.com>
2023-08-22 20:03:39 -0400

    logic for accepting dedicated texture files
    also throws an error to stop processing if there's invalid selections (iterating on behavior to reduce chance of user ending up in bad state)

--- src/hooks/useSupportedFilePicker.ts
+++ src/hooks/useSupportedFilePicker.ts
@@ -7,6 +7,15 @@ import {
   useAppSelector
 } from '@/store';
 
+/** includes character-specific super portraits and win poses */
+const DEDICATED_TEXTURE_FILE_REGEX = /^PL[0-9A-Z]{2}_(FAC|WIN).BIN$/i;
+
+/** polygon files which may be associated to textures */
+const POLYGON_FILE_REGEX = /^(STG|DM)[0-9A-Z]{2}POL\.BIN$/i;
+
+/** textures which must be associated with polygons */
+const TEXTURE_FILE_REGEX = /^(STG|DM)[0-9A-Z]{2}TEX(.modnao)?\.BIN$/i;
+
 /**
  * handle a user selection of a file client-side
  * in order to dispatch action to stream that data
@@ -29,36 +38,59 @@ export default function useSupportedFilePicker(
   });
 
   useEffect(() => {
+    const handleError = (error: string) => {
+      onError(error);
+      throw new Error(error);
+    };
     if (!plainFiles[0]) {
       return;
     }
 
     let selectedPolygonFile: File | undefined = undefined;
     let selectedTextureFile: File | undefined = undefined;
+    let dedicatedTextureFile: File | undefined = undefined;
 
-    plainFiles.forEach((f) => {
-      if (f.name.match(/^(STG|DM)[0-9A-Z]{2}POL\.BIN$/)) {
+    const DEDICATED_TEXTURE_FILE_ERROR =
+      'Cannot select other files with dedicated texture files at this time';
+
+    plainFiles.forEach((f, i) => {
+      if (dedicatedTextureFile) {
+        handleError(DEDICATED_TEXTURE_FILE_ERROR);
+        return;
+      }
+
+      if (f.name.match(DEDICATED_TEXTURE_FILE_REGEX)) {
+        if (i > 0) {
+          handleError(DEDICATED_TEXTURE_FILE_ERROR);
+          return;
+        }
+
+        dedicatedTextureFile = f;
+        return;
+      }
+
+      if (f.name.match(POLYGON_FILE_REGEX)) {
         if (!selectedPolygonFile) {
           selectedPolygonFile = f;
         } else {
-          onError('Cannot select more than one polygon file at this time');
+          onError('Cannot select more than one polygon file at a time');
           return;
         }
       }
 
-      if (f.name.match(/^(STG|DM)[0-9A-Z]{2}TEX(.modnao)?\.BIN$/)) {
+      if (f.name.match(TEXTURE_FILE_REGEX)) {
         if (!selectedTextureFile) {
           selectedTextureFile = f;
         } else {
-          onError('Cannot select more than one texture file at this time');
+          onError('Cannot select more than one texture file');
           return;
         }
       }
     });
 
-    if (!selectedPolygonFile && !selectedTextureFile) {
+    if (!selectedPolygonFile && !selectedTextureFile && !dedicatedTextureFile) {
       onError(
-        'Invalid stage file selected; Please select a file in the form STG**TEX.BIN or STG**POL.BIN'
+        'Invalid stage file selected; Please select a file in the form STG**POL.BIN along with STG**TEX.BIN, or PL**_FAC.BIN or PL**_WIN.BIN'
       );
       return;
     }
