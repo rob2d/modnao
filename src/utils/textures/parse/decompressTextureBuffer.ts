9015289d22abfe998e4b477e2a5e700d3718c3f4
Robert Concepcion III <robert.concepcion.iii@gmail.com>
2023-08-28 16:32:31 -0400

    simplify and iterate on decompression logic

--- src/utils/textures/parse/decompressTextureBuffer.ts
+++ src/utils/textures/parse/decompressTextureBuffer.ts
@@ -11,45 +11,43 @@ export default function decompressTextureBuffer(bufferPassed: Buffer) {
   let bitmask = 0b0;
   let wordsBackCount = 0;
   let grabWordCount = 0;
-  let extraWordCount = 0;
   let chunk = 0;
 
   for (let i = 0; i < buffer.length / WORD_SIZE; i++) {
-    let value = buffer.readUInt16LE(i * WORD_SIZE);
+    let word = buffer.readUInt16LE(i * WORD_SIZE);
 
     if (applyBitMask) {
-      bitmask = value;
+      bitmask = word;
       applyBitMask = false;
       continue;
     }
 
-    // reset this to be diffed on each loop
-    extraWordCount = 0;
     const isCompressed = bitmask & (COMPRESSION_FLAG >> chunk);
+    let extraWordCount = 0;
 
     if (!isCompressed) {
-      output.push(value);
-    } else if (value === 0) {
+      output.push(word);
+    } else if (word === 0) {
       break;
     } else {
       // check that only the lower 11-bits are used;
       // this says it is a 4 byte value, where the number
       // of words back to go is value & 0x7ff
-      const is32Bit = (value & 0b0111_1111_1111) === value;
+      const is32Bit = (word & 0b0111_1111_1111) === word;
 
       if (!is32Bit) {
         // the number of words to grab are in the 5 MSb
-        grabWordCount = (value >> 11) & 0b1111_1;
+        grabWordCount = (word >> 11) & 0b1111_1;
         // the number of words to go back are in the 11 LSb */
-        wordsBackCount = value & 0b0111_1111_1111;
+        wordsBackCount = word & 0b0111_1111_1111;
       } else {
-        wordsBackCount = value;
+        wordsBackCount = word;
 
         // advance/read an extra 2 bytes
         grabWordCount = buffer.readUInt16LE(++i * WORD_SIZE);
 
         // value now becomes 32-bit
-        value = (value << 16) | grabWordCount;
+        word = (word << 16) | grabWordCount;
       }
 
       if (wordsBackCount < grabWordCount) {
@@ -57,14 +55,13 @@ export default function decompressTextureBuffer(bufferPassed: Buffer) {
         grabWordCount = wordsBackCount;
       }
 
-      const nextWordPattern = [];
-
-      for (let j = 0; j < grabWordCount; j++) {
-        nextWordPattern.push(output[output.length - wordsBackCount + j]);
-      }
+      const appendedSequence = output.slice(
+        output.length - wordsBackCount,
+        output.length - wordsBackCount + grabWordCount
+      );
 
       for (let j = 0; j < grabWordCount + extraWordCount; j++) {
-        output.push(nextWordPattern[j % nextWordPattern.length]);
+        output.push(appendedSequence[j % appendedSequence.length]);
       }
     }
     chunk += 1;
