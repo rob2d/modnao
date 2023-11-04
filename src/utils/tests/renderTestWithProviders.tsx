bf992b1245fc1e96c21b435b3c2c11ae63cc3858
Robert Concepcion III <robert.concepcion.iii@gmail.com>
2023-11-04 17:49:07 -0400

    add ThemeProvider to test provider util

--- src/utils/tests/renderTestWithProviders.tsx
+++ src/utils/tests/renderTestWithProviders.tsx
@@ -1,10 +1,13 @@
-import React, { PropsWithChildren } from 'react';
+import React, { PropsWithChildren, useContext } from 'react';
+import { Provider } from 'react-redux';
 import { render } from '@testing-library/react';
 import type { RenderOptions } from '@testing-library/react';
 import type { PreloadedState } from '@reduxjs/toolkit';
-import { Provider } from 'react-redux';
+import { ThemeProvider } from '@mui/material/styles';
 
+import useUserTheme from '@/theming/useUserTheme';
 import { type AppState, type AppStore, setupStore } from '@/store';
+import ViewOptionsContext from '@/contexts/ViewOptionsContext';
 
 interface ExtendedRenderOptions extends Omit<RenderOptions, 'queries'> {
   preloadedState?: PreloadedState<AppState>;
@@ -20,8 +23,23 @@ export default function renderWithProviders(
     ...renderOptions
   }: ExtendedRenderOptions = {}
 ) {
+  function ThemedContent({
+    children
+  }: PropsWithChildren<unknown>): JSX.Element {
+    const viewOptions = useContext(ViewOptionsContext);
+    const theme = useUserTheme(
+      viewOptions.scenePalette || {},
+      viewOptions.themeKey
+    );
+    return <ThemeProvider theme={theme}>{children}</ThemeProvider>;
+  }
+
   function Wrapper({ children }: PropsWithChildren<unknown>): JSX.Element {
-    return <Provider store={store}>{children}</Provider>;
+    return (
+      <Provider store={store}>
+        <ThemedContent>{children}</ThemedContent>
+      </Provider>
+    );
   }
 
   // Return an object with the store and all of RTL's query functions
