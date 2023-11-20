b85dcb4fcfb35165070deba855c16f9bbc18a078
Robert Concepcion III <robert.concepcion.iii@gmail.com>
2023-11-19 19:16:17 -0500

    add test for loading from local storage settings

--- src/contexts/ViewOptionsContext.spec.tsx
+++ src/contexts/ViewOptionsContext.spec.tsx
@@ -5,6 +5,7 @@ import ViewOptionsContext, {
   ViewOptionsContextProvider
 } from './ViewOptionsContext';
 import { useContext } from 'react';
+import { StorageKeys } from '@/constants/StorageKeys';
 
 const viewOptionsKeys = Object.keys(defaultValues) as (keyof ViewOptions)[];
 const getOptionRenderKeys = (viewOptions: ViewOptions) =>
@@ -23,8 +24,8 @@ function TestOptionsContext() {
 }
 
 describe('ViewOptionsContext', () => {
-  it('has all values initialized properly', async () => {
-    await render(
+  it('has all values initialized properly', () => {
+    render(
       <ViewOptionsContextProvider>
         <TestOptionsContext />
       </ViewOptionsContextProvider>
@@ -35,6 +36,40 @@ describe('ViewOptionsContext', () => {
     );
 
     displayedDefaults.forEach((d) =>
+      expect(screen.getByText(d)).toBeInTheDocument()
+    );
+  });
+
+  it('has relevant values in localStorage set when initialized', () => {
+    window.localStorage.setItem(StorageKeys.MESH_DISPLAY_MODE, 'textured');
+    window.localStorage.setItem(StorageKeys.WIREFRAME_LINE_WIDTH, '5');
+    window.localStorage.setItem(StorageKeys.AXES_HELPER_VISIBLE, 'false');
+    window.localStorage.setItem(StorageKeys.OBJECT_ADDRESSES_VISIBLE, 'true');
+    window.localStorage.setItem(StorageKeys.THEME_KEY, 'dark');
+    window.localStorage.setItem(StorageKeys.DISABLE_BACKFACE_CULLING, 'true');
+    window.localStorage.setItem(StorageKeys.UV_REGIONS_HIGHLIGHTED, 'false');
+    window.localStorage.setItem(StorageKeys.DEV_OPTIONS_VISIBLE, 'true');
+    window.localStorage.setItem(StorageKeys.ENABLE_VERTEX_COLORS, 'false');
+
+    render(
+      <ViewOptionsContextProvider>
+        <TestOptionsContext />
+      </ViewOptionsContextProvider>
+    );
+
+    const displayedValues = [
+      'meshDisplayMode: textured',
+      'wireframeLineWidth: 5',
+      'axesHelperVisible: false',
+      'objectAddressesVisible: true',
+      'themeKey: dark',
+      'disableBackfaceCulling: true',
+      'uvRegionsHighlighted: false',
+      'devOptionsVisible: true',
+      'enableVertexColors: false'
+    ];
+
+    displayedValues.forEach((d) =>
       expect(screen.getByText(d)).toBeInTheDocument()
     );
   });
