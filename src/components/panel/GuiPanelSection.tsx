7d1a55d3e67c45e26a0432bf8ea0ba765f8429f7
rob2d <robert.concepcion.iii@gmail.com>
2024-07-05 12:19:58 -0400

    add controls to collapsed Models panel & some missing tooltips

--- src/components/panel/GuiPanelSection.tsx
+++ src/components/panel/GuiPanelSection.tsx
@@ -9,7 +9,12 @@ import {
 } from '@mui/material';
 import Icon from '@mdi/react';
 
-const StyledSubheader = styled(ListSubheader)(`& {
+const StyledSubheader = styled(ListSubheader)(
+  ({ theme }) => `
+
+  & {
+  }
+  .section-header {
     display: flex;
     align-items: center;
     justify-content: space-between;
@@ -19,38 +24,62 @@ const StyledSubheader = styled(ListSubheader)(`& {
   .header-text {
     display: flex;
     flex-direction: column;
-  }`);
+  }
+    
+  & .collapsed-content {
+    margin-top: ${theme.spacing(1)};
+  }
+  `
+);
 
 type Props = {
   children: React.ReactNode;
   title: string;
   subtitle?: string;
+  collapsedContent?: React.ReactNode;
+  collapsedContentFABs?: React.ReactNode[];
 };
-export default function GuiPanelSection({ children, title, subtitle }: Props) {
+
+export default function GuiPanelSection({
+  children,
+  title,
+  subtitle,
+  collapsedContent,
+  collapsedContentFABs
+}: Props) {
   const [isExpanded, setExpanded] = useState(true);
   const toggleContentExpanded = useCallback(
     () => setExpanded(!isExpanded),
     [isExpanded]
   );
+
   return (
     <>
       <StyledSubheader id='nested-list-subheader'>
-        <div className={'.header-text'}>
-          <Typography variant='subtitle2'>{title}</Typography>
-          {!subtitle ? undefined : (
-            <Typography variant='caption'>{subtitle}</Typography>
-          )}
+        <div className='section-header'>
+          <div className='header-text'>
+            <Typography variant='subtitle2'>{title}</Typography>
+            {!subtitle ? undefined : (
+              <Typography variant='caption'>{subtitle}</Typography>
+            )}
+          </div>
+          {isExpanded ? undefined : collapsedContentFABs}
+          <Tooltip title={`${isExpanded ? 'Collapse' : 'Expand'} section`}>
+            <IconButton onClick={toggleContentExpanded}>
+              <Icon
+                path={
+                  !isExpanded
+                    ? mdiUnfoldLessHorizontal
+                    : mdiUnfoldMoreHorizontal
+                }
+                size={1}
+              />
+            </IconButton>
+          </Tooltip>
         </div>
-        <Tooltip title={`${isExpanded ? 'Collapse' : 'Expand'} section`}>
-          <IconButton onClick={toggleContentExpanded}>
-            <Icon
-              path={
-                isExpanded ? mdiUnfoldLessHorizontal : mdiUnfoldMoreHorizontal
-              }
-              size={1}
-            />
-          </IconButton>
-        </Tooltip>
+        {!isExpanded ? (
+          <div className='collapsed-content'>{collapsedContent}</div>
+        ) : undefined}
       </StyledSubheader>
       {!isExpanded ? undefined : children}
     </>
