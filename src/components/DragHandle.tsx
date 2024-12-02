c7f067d5154dc411fbcaab4e2fb5c7ed06176237
rob2d <robert.concepcion.iii@gmail.com>
2024-12-01 20:52:35 -0500

    remove deprecated DragHandle component

--- src/components/DragHandle.tsx
+++
@@ -1,7 +0,0 @@
-import { styled } from '@mui/material';
-
-const StyledPaper = styled(DragHandle)(({ theme }) => {});
-
-export default function DragHandle() {
-  return <div className='drag-handle' />;
-}
