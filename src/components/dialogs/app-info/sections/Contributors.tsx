1285f97ae51ec5061d54663cecf872c2139fe68a
rob2d <1799905+rob2d@users.noreply.github.com>
2025-02-16 19:24:45 -0500

    fix PLFAC buffer export issue after deps update
    thanks to Stéphane Courvoisier for reporting this issue

--- src/components/dialogs/app-info/sections/Contributors.tsx
+++ src/components/dialogs/app-info/sections/Contributors.tsx
@@ -37,7 +37,7 @@ export default function Contributors() {
         <Typography variant={'subtitle1'}>User Testing & Feedback</Typography>
         <Typography variant={'body2'}>
           Magnetro2K, Paxtez, DJ Clayface, Toan, Blindfire604, derek (ateam),
-          ONTortita
+          ONTortita, Stéphane Courvoisier
         </Typography>
       </StyledContent>
     </div>
