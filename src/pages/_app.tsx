7d72cf4bfe8d68b27a22f4e7098dc4dc56c8dad8
rob2d <robert.concepcion.iii@gmail.com>
2025-02-22 16:59:28 -0500

    edge case fixes/updates from recent React18 & Mui6

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,27 +4,18 @@ import createEmotionCache from '../createEmotionCache';
 import type { AppProps } from 'next/app';
 import useUserTheme from '@/theming/useUserTheme';
 import { wrapper } from '@/store';
-import ViewOptionsContext, {
-  ViewOptionsContextProvider
-} from '@/contexts/ViewOptionsContext';
+import { ViewOptionsContextProvider } from '@/contexts/ViewOptionsContext';
 import { SceneContextProvider } from '@/contexts/SceneContext';
 import { Analytics } from '@vercel/analytics/react';
 import { Provider } from 'react-redux';
-import { useContext } from 'react';
 
 const clientSideEmotionCache = createEmotionCache();
 interface ThisAppProps extends AppProps {
   emotionCache?: EmotionCache;
 }
 
-const defaultTheme = {};
-
 const ThemedApp = ({ Component, ...props }: ThisAppProps) => {
-  const viewOptions = useContext(ViewOptionsContext);
-  const theme = useUserTheme(
-    viewOptions.scenePalette || defaultTheme,
-    viewOptions.themeKey
-  );
+  const theme = useUserTheme();
 
   return (
     <ThemeProvider theme={theme}>
