b9d8513db008460b056714d6433feae4449c948a
rob2d <>
2026-05-13 20:40:00 -0400

    update threejs/related libs, opt out of turbopack, a few fixes after updates

---
+++ src/types/three-examples.d.ts
@@ -0,0 +1,11 @@
+declare module 'three/examples/jsm/lines/Line2' {
+  export * from 'three/examples/jsm/lines/Line2.js';
+}
+
+declare module 'three/examples/jsm/lines/LineMaterial' {
+  export * from 'three/examples/jsm/lines/LineMaterial.js';
+}
+
+declare module 'three/examples/jsm/lines/LineGeometry' {
+  export * from 'three/examples/jsm/lines/LineGeometry.js';
+}
