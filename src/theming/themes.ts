b4217addd82ddcf861180c4109743a56f82ee0b0
rob2d <1799905+rob2d@users.noreply.github.com>
2026-07-12 17:05:15 -0400

    use one shared dropShadowContrast mixin, add support for Fira Code

--- src/theming/themes.ts
+++ src/theming/themes.ts
@@ -25,30 +25,50 @@ export const publicSans = localFont({
   display: 'swap'
 });
 
+export const firaCode = localFont({
+  src: [
+    {
+      path: '../../public/fonts/FiraCodeLatin400.woff2',
+      style: 'normal',
+      weight: '400'
+    },
+    {
+      path: '../../public/fonts/FiraCodeLatin700.woff2',
+      style: 'normal',
+      weight: '700'
+    }
+  ],
+  display: 'swap'
+});
+
+export const firaCodeFontFamily = firaCode.style.fontFamily;
+
 const modes: PaletteMode[] = ['dark', 'light'];
 
 type AppThemeMixin = SystemStyleObject<Theme>;
 
 interface AppThemeMixins {
+  dropShadowContrast: AppThemeMixin;
   dialogScrollEdgeFrame: AppThemeMixin;
   dialogScrollEdgeScroller: AppThemeMixin;
   fileDragActiveAfter: AppThemeMixin;
+  fontFamilyMono: CSSProperties['fontFamily'];
   guiPanelExpansionL0W: CSSProperties['width'];
   guiPanelExpansionL1W: CSSProperties['width'];
   guiPanelExpansionL2W: CSSProperties['width'];
   guiPanelExpansionL3W: CSSProperties['width'];
-  sceneIconMixin: AppThemeMixin;
 }
 
 interface AppThemeMixinsOptions {
+  dropShadowContrast?: AppThemeMixin;
   dialogScrollEdgeFrame?: AppThemeMixin;
   dialogScrollEdgeScroller?: AppThemeMixin;
   fileDragActiveAfter?: AppThemeMixin;
+  fontFamilyMono?: CSSProperties['fontFamily'];
   guiPanelExpansionL0W?: CSSProperties['width'];
   guiPanelExpansionL1W?: CSSProperties['width'];
   guiPanelExpansionL2W?: CSSProperties['width'];
   guiPanelExpansionL3W?: CSSProperties['width'];
-  sceneIconMixin?: AppThemeMixin;
 }
 
 declare module '@mui/material' {
@@ -96,6 +116,12 @@ declare module '@mui/material/styles' {
 }
 
 const mixins = {
+  dropShadowContrast: {
+    color: 'var(--mui-palette-common-white)',
+    filter:
+      'drop-shadow(2px 2px 1px color-mix(in srgb, var(--mui-palette-common-black) 70%, transparent))',
+    mixBlendMode: 'luminosity'
+  },
   dialogScrollEdgeFrame: {
     position: 'relative',
     boxSizing: 'border-box',
@@ -155,16 +181,11 @@ const mixins = {
     opacity: 0.75,
     pointerEvents: 'none'
   },
+  fontFamilyMono: firaCodeFontFamily,
   guiPanelExpansionL0W: '32px',
   guiPanelExpansionL1W: '222px',
   guiPanelExpansionL2W: '388px',
-  guiPanelExpansionL3W: '592px',
-  sceneIconMixin: {
-    color: 'var(--mui-palette-common-white)',
-    filter:
-      'drop-shadow(2px 2px 1px color-mix(in srgb, var(--mui-palette-common-black) 50%, transparent))',
-    mixBlendMode: 'luminosity'
-  }
+  guiPanelExpansionL3W: '592px'
 } satisfies AppThemeMixins;
 
 export type AppThemes = {
